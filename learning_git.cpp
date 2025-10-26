8a6e42e3240b38eaa4ef9753807364b23d44b30c
Matvey Filipchick <filipchickmatvey@gmail.com>
2025-10-26 09:22:19 +0300

    Add deviding in code

--- learning_git.cpp
+++ learning_git.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main() {
 
-    int num1{}, num2{}, sum{}, multiply{};
+    int num1{}, num2{}, sum{}, multiply{}, devide{};
     sum = num1 + num2;
     multiply = num1 * num2;
+    devide = num1 / num2;
     cout << "Im learnig git";
 
     return 0;

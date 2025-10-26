3332395f7d6a7a0e89bc1bf9f8f05ebb45ac5fff
Matvey Filipchick <filipchickmatvey@gmail.com>
2025-10-26 10:59:08 +0300

    Added second multiply of two same numbers

--- learning_git.cpp
+++ learning_git.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main() {
 
-    int num1{}, num2{}, sum{}, multiply{}, devide{}, rasnost{}, multiply_num1{};
+    int num1{}, num2{}, sum{}, multiply{}, devide{}, rasnost{}, multiply_num1{}, multiply_num2{};
     sum = num1 + num2;
     rasnost = num1 - num2;
     multiply = num1 * num2;
     devide = num1 / num2;
     multiply_num1 = num1 * num1;
+    multiply_num2 = num2 * num2;
     cout << "Im learnig git";
 
     return 0;
